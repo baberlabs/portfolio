aaf219a048f303bd9f380fbe5309f292acba8ee3
Baber Khan <bbrkhnn@outlook.com>
2024-07-14 18:43:40 +0100

    Add Hero component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
+import Hero from "../components/Hero";
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-3 bg-zinc-800 text-zinc-200">
-      <h1 className="font-black">Home</h1>
-      <p>Homepage for the portfolio website</p>
+    <main className="flex h-screen flex-col gap-3 bg-zinc-800 text-zinc-200">
+      <Hero />
     </main>
   );
 }
