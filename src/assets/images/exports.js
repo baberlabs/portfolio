69941f77a6d35519d47ae1a7abe54a075fbca65c
Baber Khan <bbrkhnn@outlook.com>
2024-07-14 18:43:01 +0100

    Add export file for all images

---
+++ src/assets/images/exports.js
@@ -0,0 +1,38 @@
+// skills
+import git from "./logos/git.svg";
+import github from "./logos/github.svg";
+import figma from "./logos/figma.svg";
+import html from "./logos/html.svg";
+import css from "./logos/css.svg";
+import javascript from "./logos/javascript.svg";
+import tailwindcss from "./logos/tailwindcss.svg";
+import react from "./logos/react.svg";
+import nodejs from "./logos/nodejs.svg";
+import expressjs from "./logos/expressjs.svg";
+import sql from "./logos/sql.svg";
+import postgresql from "./logos/postgresql.svg";
+
+// projects
+import capeRainFrog from "./screenshots/cape-rain-frog.png";
+import habitTracker from "./screenshots/habit-tracker.png";
+import taskManager from "./screenshots/task-manager.png";
+import todoApp from "./screenshots/todo-app.png";
+
+export {
+  git,
+  github,
+  figma,
+  html,
+  css,
+  javascript,
+  tailwindcss,
+  react,
+  nodejs,
+  expressjs,
+  sql,
+  postgresql,
+  capeRainFrog,
+  habitTracker,
+  taskManager,
+  todoApp,
+};
