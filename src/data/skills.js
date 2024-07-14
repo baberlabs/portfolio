581f82a06297c6bf30239d45d556db589729edde
Baber Khan <bbrkhnn@outlook.com>
2024-07-14 18:42:28 +0100

    Refactor

---
+++ src/data/skills.js
@@ -0,0 +1,20 @@
+import * as logos from "../assets/images/exports";
+
+const App = (hell, o) => {
+  return 1;
+};
+
+export default [
+  { text: "Git", icon: logos.git },
+  { text: "GitHub", icon: logos.github },
+  { text: "Figma", icon: logos.figma },
+  { text: "HTML", icon: logos.html },
+  { text: "CSS", icon: logos.css },
+  { text: "JavaScript", icon: logos.javascript },
+  { text: "Tailwind CSS", icon: logos.tailwindcss },
+  { text: "React", icon: logos.react },
+  { text: "Node.js", icon: logos.nodejs },
+  { text: "Express.js", icon: logos.expressjs },
+  { text: "SQL", icon: logos.sql },
+  { text: "PostgreSQL", icon: logos.postgresql },
+];
