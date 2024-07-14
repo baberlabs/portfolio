ad85f8523d28337b070b2a6d9d2f16b0a3eabe4b
Baber Khan <bbrkhnn@outlook.com>
2024-07-14 18:44:03 +0100

    Fix broken link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,30 +1,25 @@
-import { PROJECTS } from "../my-projects";
-import { SCREENSHOTS } from "../images";
-import { imageName, projectName } from "../functions";
+import projects from "../data/projects";
 
 export default function Projects() {
   return (
     <main className="flex h-screen flex-col gap-10 bg-zinc-800 px-[100px] py-10 text-zinc-200">
       <h1 className="text-center text-4xl font-black">Projects</h1>
       <ul className="flex flex-row flex-wrap justify-center gap-8">
-        {PROJECTS.map((project) => {
+        {projects.map((project, index) => {
           return (
-            <li
-              key={project}
-              className="flex h-fit w-[300px] flex-col shadow-xl"
-            >
+            <li key={index} className="flex h-fit w-[300px] flex-col shadow-xl">
               <a
-                href={`https://baberlabs.github.io/${project}`}
+                href={project.live}
                 className="bg-zinc-900 p-3 font-bold underline"
               >
-                {projectName(project)}
+                {project.text}
               </a>
               <img
-                src={SCREENSHOTS[imageName(project)]}
-                alt={`Screenshot of ${projectName(project)}`}
+                src={project.image}
+                alt={`Screenshot of ${project.text} Project`}
               />
               <a
-                href={`https://github.com/baberlabs/${project}`}
+                href={project.repo}
                 className="bg-zinc-800 p-2 text-right underline"
               >
                 GitHub
