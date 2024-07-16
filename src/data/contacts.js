f31109e9c93596f396597be4f0e59374215c1cdc
Baber Khan <bbrkhnn@outlook.com>
2024-07-16 21:15:40 +0100

    Create contact objects array and export it

---
+++ src/data/contacts.js
@@ -0,0 +1,30 @@
+import * as icons from "../assets/images/exports";
+
+export default [
+  {
+    text: "Discord",
+    icon: icons.discord,
+    url: "https://discord.com/users/bbrkhnn",
+  },
+  { text: "Email", icon: icons.email, url: "mailto:hello@baberr.com" },
+  // { text: "Facebook", icon: icons.facebook, url: "" },
+  {
+    text: "GitHub",
+    icon: icons.github,
+    url: "https://github.com/baberlabs",
+  },
+  // { text: "Instagram", icon: icons.instagram, url: "" },
+  {
+    text: "LinkedIn",
+    icon: icons.linkedin,
+    url: "https://linkedin.com/in/baberr",
+  },
+  { text: "Phone", icon: icons.phone, url: "tel:+447438234827" },
+  // { text: "Slack", icon: icons.slack, url: "" },
+  // { text: "Twitter", icon: icons.twitter, url: "" },
+  {
+    text: "WhatsApp",
+    icon: icons.whatsapp,
+    url: "https://wa.me/+447438234827",
+  },
+];
