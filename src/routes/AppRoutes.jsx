0de8bf9b4030227edd196060e85ad381bab69721
Baber Khan <bbrkhnn@outlook.com>
2025-06-29 19:29:38 +0100

    Remove redundant homepage

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import Layout from "../Layout";
-import HomePage from "../pages/HomePage";
 import ProjectsPage from "../pages/ProjectsPage";
 import BlogPage from "../pages/BlogPage";
 import CVPage from "../pages/CVPage";
@@ -11,10 +10,9 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<CVPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/blog" element={<BlogPage />} />
-        <Route path="/cv" element={<CVPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
